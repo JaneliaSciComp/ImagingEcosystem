8cc6db0b60e95099a39229989434f4b3dbb99b6b
Rob Svirskas <svirskasr@janelia.hhmi.org>
2017-08-01 14:06:57 -0400

    Adding output file for Amanda

--- bin/gen1_split_generator.py
+++ bin/gen1_split_generator.py
@@ -4,19 +4,21 @@ import pprint
 import re
 import urllib2
 
-RESPONDER = 'http://informatics-flask-dev.int.janelia.org:83/sage_responder/'
+SAGE_RESPONDER = 'http://informatics-flask-dev.int.janelia.org:83/sage_responder/'
+FLYCORE_RESPONDER = 'http://informatics-prod.int.janelia.org/cgi-bin/flycore_responder.cgi'
 SUFFIX_SCORE = {'AV_01': 1,
                 'AV_57': 1,
                 'BB_04': 1,
                 'BB_21': 1,
                 'XA_21': 1,
                 'XD_01': 1}
+fcdict = dict()
 
 
 def processInput():
     if (VERBOSE):
         print "Fetching split halves"
-    url = RESPONDER + "split_halves"
+    url = SAGE_RESPONDER + "split_halves"
     req = urllib2.Request(url)
     req.add_header('Content-Type', 'application/json')
     try:
@@ -41,14 +43,12 @@ def processInput():
             (ad, dbd) = generateCross(fragdict, frag1, frag2)
             if (ad and dbd):
                 crosses += 1
-                if (DEBUG):
-                    print "  Found cross %s x %s" % (ad, dbd)
-                CROSSES.write("%s x %s\n" % (ad, dbd))
+                goodCross(ad,dbd)
             elif ((not ad) or (not dbd)):
-                NO_CROSSES.write("Missing AD/DBD for %s x %s\n" % (frag1,
+                NO_CROSSES.write("Missing AD/DBD for %s-x-%s\n" % (frag1,
                                                                    frag2))
     if (VERBOSE):
-        print "Crosses found: %d" % (crosses)
+        print "  Crosses found: %d" % (crosses)
 
 
 def generateCross(fragdict, frag1, frag2):
@@ -99,29 +99,68 @@ def generateScore(line):
         return(0)
 
 
+def goodCross(ad, dbd):
+    if (DEBUG):
+        print "  Found cross %s x %s" % (ad, dbd)
+    CROSSES.write("%s x %s\n" % (ad, dbd))
+    if (ad not in fcdict):
+        flycoreData(ad)
+    if (dbd not in fcdict):
+        flycoreData(dbd)
+    alias = ad + '-x-' + dbd
+    pfrag = fcdict[ad]['fragment'] + '-x-' + fcdict[dbd]['fragment']
+    FLYCORE.write("%s\t%s\t%s\t%s\t%s" % ('Tanya Wolff','',alias,pfrag,''))
+    for w in (ad,dbd):
+        FLYCORE.write("\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" % ('Rack',fcdict[w]['__kp_UniqueID'],fcdict[w]['RobotID'],
+                                                                fcdict[w]['Genotype_GSI_Name_PlateWell'],fcdict[w]['Chromosome'],
+                                                                w,fcdict[w]['fragment'],fcdict[w]['Production_Info'],
+                                                                fcdict[w]['Quality_Control']))
+    FLYCORE.write("\n")
+
+
+def flycoreData(line):
+    url = FLYCORE_RESPONDER + "?request=linedata&line=" + line
+    req = urllib2.Request(url)
+    req.add_header('Content-Type', 'application/json')
+    try:
+        response = urllib2.urlopen(req,)
+    except urllib2.HTTPError, e:
+        print 'Call to %s failed: %s.' % (url, e.code)
+        pp.pprint(e.read())
+    else:
+        fd = json.load(response)
+        fcdict[line] = fd['linedata']
+
+
 def readLines(fragdict):
     linelist = []
     fragsFound = dict()
     frags_read = 0
     F = open(INPUT_FILE, 'r')
     for input_line in F:
+        frags_read = frags_read + 1
         search_term = input_line.rstrip()
         new_term = ''
         if search_term.isdigit():
-            st = convertVT('VT' + search_term.zfill(6))
+            vt = 'VT' + search_term.zfill(6)
+            st = convertVT(vt)
+            if (not st):
+                print "Could not convert %s to line" % (vt)
+                NO_CROSSES.write("Could not convert %s to line" % (vt))
+                continue
             if (DEBUG):
                 print "(Converted %s to %s)" % (search_term, st)
             search_term = st.split('_')[1]
         search_term = search_term.upper()
         new_term = '*' + search_term + '*'
         if (search_term in fragsFound):
+            frags_read = frags_read - 1
             continue
         else:
             fragsFound[search_term] = 1
-        frags_read = frags_read + 1
         if (DEBUG):
             print search_term + ' (' + new_term + ')'
-        url = RESPONDER + "lines?name=" + new_term + '&_columns=name'
+        url = SAGE_RESPONDER + "lines?name=" + new_term + '&_columns=name'
         req = urllib2.Request(url)
         req.add_header('Content-Type', 'application/json')
         try:
@@ -146,16 +185,16 @@ def readLines(fragdict):
     F.close()
     linelist.sort()
     if (VERBOSE):
-        print "Fragments read: %d" % (frags_read)
+        print "  Fragments read: %d" % (frags_read)
         n = len(linelist)
-        print "Eligible line fragments: %d" % n
+        print "  Eligible line fragments: %d" % n
         combos = (n * (n - 1)) / 2
-        print "Theoretical crosses: %d" % (combos)
+        print "  Theoretical crosses: %d" % (combos)
     return(linelist)
 
 
 def convertVT(vt):
-    url = RESPONDER + "translatevt/" + vt
+    url = SAGE_RESPONDER + "translatevt/" + vt
     req = urllib2.Request(url)
     req.add_header('Content-Type', 'application/json')
     try:
@@ -165,7 +204,7 @@ def convertVT(vt):
         pp.pprint(e.read())
     else:
         ld = json.load(response)
-        if ('line_data' in ld):
+        if ('line_data' in ld and len(ld['line_data'])):
             return(ld['line_data'][0]['line'])
         else:
             return('')
@@ -190,7 +229,18 @@ if __name__ == '__main__':
         VERBOSE = True
     pp = pprint.PrettyPrinter(indent=4)
     CROSSES = open(INPUT_FILE + '.crosses.txt', 'w')
+    FLYCORE = open(INPUT_FILE + '.flycore.xls', 'w')
+    for h in ('Who', '#', 'Alias', 'Pfrag'):
+        FLYCORE.write("%s\t" % (h))
+    FLYCORE.write('IS')
+    for i in (1,2):
+        for h in ('__flipper_flystocks_stock::RACK_LOCATION','StockFinder::__kp_UniqueID','StockFinder::RobotID',
+                  'StockFinder::Genotype_GSI_Name_PlateWell','StockFinder::Chromosome','StockFinder::Stock_Name',
+                  'StockFinder::fragment','StockFinder::Production_Info','StockFinder::Quality_Control'):
+            FLYCORE.write("\t%s" % (h))
+    FLYCORE.write("\n")
     NO_CROSSES = open(INPUT_FILE + '.no_crosses.txt', 'w')
     processInput()
     CROSSES.close()
+    FLYCORE.close()
     NO_CROSSES.close()
