65501debc784262ab291b2a8fe07df487ae14c1d
robsv <svirskasr@janelia.hhmi.org>
2017-08-17 17:05:42 -0400

    Initial load of gen1_autoselect.pl

--- bin/gen1_autoselect.pl
+++ bin/gen1_autoselect.pl
@@ -18,6 +18,7 @@ use constant DATA_PATH => '/opt/informatics/data/';
 # * Constants                                                                *
 # ****************************************************************************
 my %CONFIG;
+my($DEBUG,$VERBOSE,$WRITE) = (0)x3;
 
 
 # ****************************************************************************
@@ -66,24 +67,111 @@ sub processLines
   }
   foreach my $sl (sort keys %line) {
     print "$sl\n";
-    # Skip if we don't have a brain
-    unless (exists $line{$sl}{Brain}) {
-      print "  Missing brain for $sl\n";
-      next;
+    my($error,$vi) = (0)x2;
+    # Skip if we don't have a brain or VNC
+    foreach (qw(Brain VNC)) {
+      unless (exists $line{$sl}{$_}) {
+        &notify("Missing $_ for $sl");
+        $error++;
+      }
     }
+    next if ($error);
     # Skip annotated lines
     if ($line{$sl}{Annotated}{Brain} && $line{$sl}{Annotated}{VNC}) {
       print "  Line $sl is already annotated\n";
       next;
     }
-    # Choose a line/image
-    if ((scalar @{$line{$sl}{Brain}} == 1) && ($line{$sl}{Brain}[0]{vt_status} ne 'VT mismatch')) {
+    # Choose a brain line/image
+    if (scalar @{$line{$sl}{Brain}} == 1) {
+      # Only one brain to choose from
       my $line = $line{$sl}{Brain}[0]{line};
       my $image = $line{$sl}{Brain}[0]{id};
-      print "  Choose brain $line $image $line{$sl}{Brain}[0]{vt_status}\n";
-      next;
+      if ($line{$sl}{Brain}[0]{vt_status} ne 'VT mismatch') {
+        # It's not a mismatch
+        print "  Choose brain $line $image ($line{$sl}{Brain}[0]{vt_status})\n";
+        ($error,$vi) = &chooseVNC($line{$sl}{VNC},$line{$sl}{Brain}[0]{id});
+        ($error) ? &notify($error) : print "  Choose VNC $line{$sl}{VNC}[$vi]{line} $line{$sl}{VNC}[$vi]{id} ($line{$sl}{VNC}[$vi]{vt_status})\n";
+        if (!$error) {
+          print "  Publish $line/$image $line{$sl}{VNC}[$vi]{line}/$line{$sl}{VNC}[$vi]{id}\n";
+        }
+        next;
+      }
+      else {
+        # It's a mismatch
+        &notify("Can't use brain $line $image ($line{$sl}{Brain}[0]{vt_status})");
+        next;
+      }
+    }
+    else {
+      # More than one brain
+      my $max_qi = 0;
+      my $brain_index = -1;
+      my $mm = 0;
+      my ($image,$line);
+      foreach (0..scalar(@{$line{$sl}{Brain}})-1) {
+        $line = $line{$sl}{Brain}[$_]{line};
+        $image = $line{$sl}{Brain}[$_]{id};
+        my $qi = $line{$sl}{Brain}[$_]{qi} || 0;
+        if (($line{$sl}{Brain}[$_]{vt_status} ne 'VT mismatch')
+            && ($qi > $max_qi)) {
+          $max_qi = $qi;
+          $brain_index = $_;
+        }
+        elsif ($line{$sl}{Brain}[$_]{vt_status} eq 'VT mismatch') {
+          $mm++;
+        }
+      }
+      if ($max_qi) {
+        print "  Choose multibrain $line $image ($line{$sl}{Brain}[$brain_index]{vt_status})\n";
+        ($error,$vi) = &chooseVNC($line{$sl}{VNC},$line{$sl}{Brain}[$brain_index]{id});
+        ($error) ? &notify($error) : print "  Choose VNC $line{$sl}{VNC}[$vi]{line} $line{$sl}{VNC}[$vi]{id} ($line{$sl}{VNC}[$vi]{vt_status})\n";
+        if (!$error) {
+          print "  Publish $line/$image $line{$sl}{VNC}[$vi]{line}/$line{$sl}{VNC}[$vi]{id}\n";
+        }
+        next;
+      }
+      elsif ($mm eq scalar(@{$line{$sl}{Brain}})) {
+        &notify("All brains are VT mismatches for $sl");
+        next;
+      }
+      else {
+        &notify("Could not find brains with Qi scores for $sl");
+        next;
+      }
+    }
+    print "  Dead end for $sl\n";
+  }
+}
+
+
+sub chooseVNC
+{
+  my($vncarr,$brain_id) = @_;
+  if (scalar(@$vncarr) == 1) {
+    # One VNC
+    my $line = $vncarr->[0]{line};
+    my $image = $vncarr->[0]{id};
+    if ($vncarr->[0]{vt_status} ne 'VT mismatch') {
+      return((0)x2);
+    }
+    else {
+      return("Can't use VNC $line $image ($vncarr->[0]{vt_status})",0);
     }
   }
+  else {
+    # Multiple VNCs
+    foreach (0..scalar(@$vncarr)-1) {
+      return(0,$_) unless ($vncarr->[$_]{vt_status} eq 'VT mismatch');
+    }
+    return("Can't find VNC with good VT status",0);
+  }
+}
+
+
+sub notify
+{
+  my($msg) = shift;
+  print "  $msg\n";
 }
 
 
