283ee8f840a8b0e1858675c5d9edc06c94c5248e
robsv <svirskasr@janelia.hhmi.org>
2017-09-19 11:23:49 -0400

    Latest changes to gen1_autoselect

--- bin/gen1_autoselect.pl
+++ bin/gen1_autoselect.pl
@@ -3,7 +3,10 @@
 use strict;
 use warnings;
 use Data::Dumper;
+use Date::Parse;
+use Getopt::Long;
 use JSON;
+use IO::File;
 use LWP::Simple;
 use POSIX qw(ceil);
 use Switch;
@@ -18,7 +21,13 @@ use constant DATA_PATH => '/opt/informatics/data/';
 # * Constants                                                                *
 # ****************************************************************************
 my %CONFIG;
+my $SESSION = 'http://informatics-flask.int.janelia.org:83/sage_responder/session';
+
+# ****************************************************************************
+# * Variables                                                                *
+# ****************************************************************************
 my($DEBUG,$VERBOSE,$WRITE) = (0)x3;
+my $error_handle;
 
 
 # ****************************************************************************
@@ -26,6 +35,8 @@ my($DEBUG,$VERBOSE,$WRITE) = (0)x3;
 # ****************************************************************************
 sub initializeProgram
 {
+  my $filename = time . '_gen1_autoselect_errors.txt';
+  $error_handle = new IO::File ">$filename";
   # Get WS REST config
   my $file = DATA_PATH . 'rest_services.json';
   open SLURP,$file or &terminateProgram("Can't open $file: $!");
@@ -78,7 +89,7 @@ sub processLines
     next if ($error);
     # Skip annotated lines
     if ($line{$sl}{Annotated}{Brain} && $line{$sl}{Annotated}{VNC}) {
-      print "  Line $sl is already annotated\n";
+      print "  Line $sl is already annotated\n" if ($VERBOSE);
       next;
     }
     # Choose a brain line/image
@@ -88,11 +99,16 @@ sub processLines
       my $image = $line{$sl}{Brain}[0]{id};
       if ($line{$sl}{Brain}[0]{vt_status} ne 'VT mismatch') {
         # It's not a mismatch
-        print "  Choose brain $line $image ($line{$sl}{Brain}[0]{vt_status})\n";
-        ($error,$vi) = &chooseVNC($line{$sl}{VNC},$line{$sl}{Brain}[0]{id});
-        ($error) ? &notify($error) : print "  Choose VNC $line{$sl}{VNC}[$vi]{line} $line{$sl}{VNC}[$vi]{id} ($line{$sl}{VNC}[$vi]{vt_status})\n";
+        print "  Choose brain $line $image ($line{$sl}{Brain}[0]{vt_status})\n" if ($DEBUG);
+        ($error,$vi) = &chooseVNC($line{$sl}{VNC},,$line{$sl}{Brain}[0]{capture_date});
+        if ($error) {
+          &notify($error);
+        }
+        elsif ($DEBUG) {
+          print "  Choose VNC $line{$sl}{VNC}[$vi]{line} $line{$sl}{VNC}[$vi]{id} ($line{$sl}{VNC}[$vi]{vt_status})\n";
+        }
         if (!$error) {
-          print "  Publish $line/$image $line{$sl}{VNC}[$vi]{line}/$line{$sl}{VNC}[$vi]{id}\n";
+          &publishSet($line{$sl}{Brain}[0],$line{$sl}{VNC}[$vi]);
         }
         next;
       }
@@ -122,16 +138,21 @@ sub processLines
         }
       }
       if ($max_qi) {
-        print "  Choose multibrain $line $image ($line{$sl}{Brain}[$brain_index]{vt_status})\n";
-        ($error,$vi) = &chooseVNC($line{$sl}{VNC},$line{$sl}{Brain}[$brain_index]{id});
-        ($error) ? &notify($error) : print "  Choose VNC $line{$sl}{VNC}[$vi]{line} $line{$sl}{VNC}[$vi]{id} ($line{$sl}{VNC}[$vi]{vt_status})\n";
+        print "  Choose multibrain $line $image ($line{$sl}{Brain}[$brain_index]{vt_status})\n" if ($DEBUG);
+        ($error,$vi) = &chooseVNC($line{$sl}{VNC},$line{$sl}{Brain}[$brain_index]{capture_date});
+        if ($error) {
+          &notify($error);
+        }
+        elsif ($DEBUG) {
+          print "  Choose VNC $line{$sl}{VNC}[$vi]{line} $line{$sl}{VNC}[$vi]{id} ($line{$sl}{VNC}[$vi]{vt_status})\n";
+        }
         if (!$error) {
-          print "  Publish $line/$image $line{$sl}{VNC}[$vi]{line}/$line{$sl}{VNC}[$vi]{id}\n";
+          &publishSet($line{$sl}{Brain}[$brain_index],$line{$sl}{VNC}[$vi]);
         }
         next;
       }
       elsif ($mm eq scalar(@{$line{$sl}{Brain}})) {
-        &notify("All brains are VT mismatches for $sl");
+        &notify("Can't use brains from $sl (all are mismatches)");
         next;
       }
       else {
@@ -146,7 +167,7 @@ sub processLines
 
 sub chooseVNC
 {
-  my($vncarr,$brain_id) = @_;
+  my($vncarr,$brain_cd) = @_;
   if (scalar(@$vncarr) == 1) {
     # One VNC
     my $line = $vncarr->[0]{line};
@@ -160,11 +181,76 @@ sub chooseVNC
   }
   else {
     # Multiple VNCs
+    my $brain_time = str2time($brain_cd);
+    my $min_delta = 1e9;
+    my $vnc_index = -1;
     foreach (0..scalar(@$vncarr)-1) {
-      return(0,$_) unless ($vncarr->[$_]{vt_status} eq 'VT mismatch');
+      next if ($vncarr->[$_]{vt_status} eq 'VT mismatch');
+      return(0,$_) unless ($brain_cd);
+      my $vnc_time = str2time($vncarr->[$_]{capture_date});
+      my $delta = abs($vnc_time - $brain_time);
+      print "  Compare times $vncarr->[$_]{id} ($vncarr->[$_]{capture_date} $vnc_time) to $brain_time: $delta\n"
+      if ($DEBUG);
+      if ($delta < $min_delta) {
+        $min_delta = $delta;
+        $vnc_index = $_;
+      }
     }
-    return("Can't find VNC with good VT status",0);
+    if ($vnc_index == -1) {
+      return("Can't find VNC with good VT status",0);
+    }
+    else {
+      return(0,$vnc_index);
+    }
   }
+}
+
+
+sub publishSet
+{
+  my($brain,$vnc) = @_;
+  if ($VERBOSE) {
+    printf "  Publish Brain $brain->{line},$brain->{name}\n";
+    printf "          VNC $vnc->{line},$vnc->{name}\n";
+  }
+  return unless ($WRITE);
+  my $msg = &publish($brain);
+  if ($msg) {
+    &notify($msg);
+    return();
+  }
+  $msg = &publish($vnc);
+  &notify($msg) if ($msg);
+}
+
+sub publish
+{
+  my($info) = @_;
+  my $rest = $CONFIG{sage}{url}.'lines?name=' . $info->{line} . '&_columns=id';
+  my $response = get $rest;
+  &terminateProgram("REST GET returned null response (Request: $rest)")
+    unless (length($response));
+  my $rvar;
+  eval {$rvar = decode_json($response)};
+  return("Could not find ID for $info->{line}") unless ($rvar->{line_data}[0]{id});
+  my $ua = LWP::UserAgent->new;
+  my $req = HTTP::Request->new(POST => $SESSION);
+  $req->header('content-type' => 'application/json');
+  $req->header('access-control-allow-origin' => '*');
+  my $post_data = {cv => "flylight_public_annotation",
+                   type => "annotation_image",
+                   name => $info->{name},
+                   line_id => $rvar->{line_data}[0]{id},
+                   image_id => $info->{id},
+                   annotator => "svirskasr",
+                   lab => "flylight"};
+  my $json = JSON->new->allow_nonref;
+  $req->content($json->encode($post_data));
+  my $resp = $ua->request($req);
+  unless ($resp->{_rc} eq 200) {
+    return($resp->{_msg});
+  }
+  return();
 }
 
 
@@ -172,11 +258,16 @@ sub notify
 {
   my($msg) = shift;
   print "  $msg\n";
+  print $error_handle "$msg\n";
 }
 
 
 # ****************************************************************************
 # * Main                                                                     *
 # ****************************************************************************
+GetOptions(write        => \$WRITE,
+           verbose      => \$VERBOSE,
+           debug        => \$DEBUG);
 &initializeProgram();
 &processLines();
+$error_handle->close();
