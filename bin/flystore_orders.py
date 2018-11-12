2155b836247a8ee18cea0add794cdd20a22827cd
Rob Svirskas <svirskasr@janelia.hhmi.org>
2018-11-12 16:37:30 -0500

    Initial load flystore_orders.py

---
+++ bin/flystore_orders.py
@@ -0,0 +1,132 @@
+#!/opt/python/bin/python2.7
+
+import argparse
+import json
+import sys
+import colorlog
+import requests
+from kafka import KafkaConsumer
+from datetime import datetime
+from smtplib import SMTPException, SMTP
+from email.mime.text import MIMEText
+
+
+# Configuration
+CONFIG = {'config': {'url': 'http://config.int.janelia.org/'}}
+SENDER = 'svirskasr@janelia.hhmi.org'
+
+
+# -----------------------------------------------------------------------------
+def call_responder(server, endpoint):
+    url = CONFIG[server]['url'] + endpoint
+    try:
+        req = requests.get(url)
+    except requests.exceptions.RequestException as err:
+        logger.critical(err)
+        sys.exit(-1)
+    if req.status_code == 200:
+        return req.json()
+    else:
+        logger.error('Status: %s', str(req.status_code))
+        sys.exit(-1)
+
+
+def initialize_program():
+    """ Initialize database """
+    global CONFIG
+    data = call_responder('config', 'config/rest_services')
+    CONFIG = data['config']
+
+
+def generate_email_message(sender, recievers, message):
+    msg = MIMEText(message)
+    msg['Subject'] = 'Split screen orders'
+    msg['From'] = sender
+    msg['To'] = ", ".join(recievers)
+    return msg.as_string()
+
+
+def send_mail(sender, recievers, message):
+    email = generate_email_message(sender, recievers, message)
+    try:
+        smtpObj = SMTP()
+        smtpObj.connect()
+        smtpObj.sendmail(sender, recievers, email)
+        logging.info("Successfully sent email")
+    except SMTPException:
+        logging.error("Error: unable to send email")
+
+
+def read_messages():
+    if ARG.server:
+        server_list = [ARG.server + ':9092']
+    else:
+        rest = call_responder('config','config/servers/Kafka')
+        server_list = rest['config']['broker_list']
+    consumer = KafkaConsumer('screen_review',
+                             bootstrap_servers=server_list,
+                             group_id=None,
+                             auto_offset_reset='earliest',
+                             consumer_timeout_ms=int(500))
+    orderlist = list()
+    for message in consumer:
+        msg = json.loads(message.value)
+        if msg['user'] != ARG.USER or msg['category'] != 'order' or message.key == None:
+            continue
+        order_date = message.key.split(' ')
+        if order_date[0] < ARG.START or order_date[0] > ARG.END:
+            continue
+        if ARG.DEBUG:
+            print(msg)
+        splittype = list()
+        for typ in ('stabilization', 'polarity', 'mcfo'):
+            if typ in msg and msg[typ]:
+                splittype.append(typ)
+        orderlist.append("%s: %s\t%s" % (message.key, msg['line'], ', '.join(splittype)))
+    if ARG.START == ARG.END:
+        body = 'On ' + ARG.START
+    else:
+        body = "Between the dates of %s and %s" % (ARG.START, ARG.END)
+    body += ", you ordered the following stable splits:\n\n"
+    for order in orderlist:
+        body += order + "\n"
+    if ARG.VERBOSE:
+        print(body)
+    send_mail(SENDER, [SENDER], body)
+
+
+# -----------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    today = datetime.today().strftime('%Y-%m-%d')
+    PARSER = argparse.ArgumentParser(
+        description='Find and index/discover newly tmogged imagery')
+    PARSER.add_argument('--server', dest='server', default='', help='Kafka server')
+    PARSER.add_argument('--verbose', action='store_true',
+                        dest='VERBOSE', default=False,
+                        help='Turn on verbose output')
+    PARSER.add_argument('--debug', action='store_true',
+                        dest='DEBUG', default=False,
+                        help='Turn on debug output')
+    PARSER.add_argument('--user', dest='USER', default='',
+                        help='User ID')
+    PARSER.add_argument('--start', dest='START', default=today,
+                        help='Start date (YYYY-MM-DD)')
+    PARSER.add_argument('--end', dest='END', default=today,
+                        help='End date (YYYY-MM-DD)')
+    ARG = PARSER.parse_args()
+
+    logger = colorlog.getLogger()
+    if ARG.DEBUG:
+        logger.setLevel(colorlog.colorlog.logging.DEBUG)
+    elif ARG.VERBOSE:
+        logger.setLevel(colorlog.colorlog.logging.INFO)
+    else:
+        logger.setLevel(colorlog.colorlog.logging.WARNING)
+    HANDLER = colorlog.StreamHandler()
+    HANDLER.setFormatter(colorlog.ColoredFormatter())
+    logger.addHandler(HANDLER)
+
+    initialize_program()
+    read_messages()
+    sys.exit(0)
