54a591148b751594de4eb7185daf808cae027b3a
robsv <svirskasr@janelia.hhmi.org>
2020-12-02 13:46:23 -0500

    Added option to load file of IDs

--- bin/indexing_cleanup.py
+++ bin/indexing_cleanup.py
@@ -11,26 +11,30 @@ from pymongo import MongoClient
 
 # SQL statements
 SQL = {
-    'OVERDUE': "SELECT i.family,ipd.value,ips.value,i.name FROM image_vw i "
-        + "JOIN image_property_vw ipd ON (i.id=ipd.image_id AND "
-        + "ipd.type='data_set') JOIN image_property_vw ips ON "
-        + "(i.id=ips.image_id AND ips.type='slide_code') WHERE i.family NOT "
-        + "LIKE 'simpson%' AND i.id NOT IN (SELECT image_id FROM "
-        + "image_property_vw WHERE type='bits_per_sample') AND "
-        + "TIMESTAMPDIFF(HOUR,i.create_date,NOW()) > 8",
-    'ALL': "SELECT i.family,ipd.value,ips.value,i.name FROM image_vw i JOIN "
-        + "image_property_vw ipd ON (i.id=ipd.image_id AND "
-        + "ipd.type='data_set') JOIN image_property_vw ips ON "
-        + "(i.id=ips.image_id AND ips.type='slide_code') WHERE i.family NOT "
-        + "LIKE 'simpson%' AND i.id NOT IN (SELECT image_id FROM "
-        + "image_property_vw WHERE type='bits_per_sample')",
+    'OVERDUE': "SELECT i.family,ipd.value AS data_set,ips.value AS slide_code,"
+               + "i.name FROM image_vw i JOIN image_property_vw ipd ON "
+               + "(i.id=ipd.image_id AND ipd.type='data_set') JOIN image_property_vw ips "
+               + "ON (i.id=ips.image_id AND ips.type='slide_code') WHERE i.family NOT "
+               + "LIKE 'simpson%' AND i.id NOT IN (SELECT image_id FROM "
+               + "image_property_vw WHERE type='bits_per_sample') AND "
+               + "TIMESTAMPDIFF(HOUR,i.create_date,NOW()) > 8",
+    'ALL': "SELECT i.family,ipd.value AS data_set,ips.value AS slide_code,"
+           + "i.name FROM image_vw i JOIN image_property_vw ipd ON "
+           + "(i.id=ipd.image_id AND ipd.type='data_set') JOIN image_property_vw ips "
+           + "ON  (i.id=ips.image_id AND ips.type='slide_code') WHERE i.family NOT "
+           + "LIKE 'simpson%' AND i.id NOT IN (SELECT image_id FROM "
+           + "image_property_vw WHERE type='bits_per_sample')",
+    'SINGLE': "SELECT family,data_set,slide_code,name FROM image_data_mv WHERE "
+              + "id=%s",
 }
 # Counters
-count = {'failure': 0, 'found': 0, 'skipped': 0, 'success': 0}
-dsdict = {}
-indexed = {}
+COUNT = {'failure': 0, 'found': 0, 'skipped': 0, 'success': 0}
+DSDICT = {}
+INDEXED = {}
 # Configuration
 CONFIG = {'config': {'url': 'http://config.int.janelia.org/'}}
+CONN = dict()
+CURSOR = dict()
 
 # -----------------------------------------------------------------------------
 
@@ -43,18 +47,17 @@ def call_responder(server, endpoint, post=''):
         else:
             req = requests.get(url)
     except requests.exceptions.RequestException as err:
-        logger.critical(err)
+        LOGGER.critical(err)
         sys.exit(-1)
     if req.status_code in (200, 201):
         return req.json()
-    elif req.status_code == 404:
+    if req.status_code == 404:
         return ''
-    else:
-        try:
-            logger.critical('%s: %s', str(req.status_code), req.json()['rest']['message'])
-        except:
-            logger.critical('%s: %s', str(req.status_code), req.text)
-        sys.exit(-1)
+    try:
+        LOGGER.critical('%s: %s', str(req.status_code), req.json()['rest']['message'])
+    except:
+        LOGGER.critical('%s: %s', str(req.status_code), req.text)
+    sys.exit(-1)
 
 
 def sql_error(err):
@@ -65,20 +68,22 @@ def sql_error(err):
     sys.exit(-1)
 
 
-def db_connect(db):
+def db_connect(dbd):
     """ Connect to a database
         Keyword arguments:
-        db: database dictionary
+          dbd: database dictionary
+        Returns:
+          connector and cursor
     """
-    logger.debug("Connecting to %s on %s", db['name'], db['host'])
+    LOGGER.info("Connecting to %s on %s", dbd['name'], dbd['host'])
     try:
-        conn = MySQLdb.connect(host=db['host'], user=db['user'],
-                               passwd=db['password'], db=db['name'])
+        conn = MySQLdb.connect(host=dbd['host'], user=dbd['user'],
+                               passwd=dbd['password'], db=dbd['name'])
     except MySQLdb.Error as err:
         sql_error(err)
     try:
-        cursor = conn.cursor()
-        return(conn, cursor)
+        cursor = conn.cursor(MySQLdb.cursors.DictCursor)
+        return conn, cursor
     except MySQLdb.Error as err:
         sql_error(err)
 
@@ -86,60 +91,78 @@ def db_connect(db):
 def connect_databases():
     try:
         client = MongoClient('mongodb3.int.janelia.org:27017')
-        db = client.jacs
-        db.authenticate('flyportalRead', 'flyportalRead')
-        cursor = db.dataSet.find({'sageGrammarPath':{'$exists':True}}, {'_id':0,'identifier':1, 'sageConfigPath':1, 'sageGrammarPath':1})
-    except Exception as e:
-        print('Could not connect to Mongo: %s' % (e))
+        dbm = client.jacs
+        dbm.authenticate('flyportalRead', 'flyportalRead')
+        cursor = dbm.dataSet.find({'sageGrammarPath':{'$exists':True}},
+                                  {'_id':0, 'identifier':1, 'sageConfigPath':1,
+                                   'sageGrammarPath':1})
+    except Exception as err:
+        print('Could not connect to Mongo: %s' % (err))
         sys.exit(-1)
-    for ds in cursor:
-        dsdict[ds['identifier']] = {'config': ds['sageConfigPath'],
-                                    'grammar': ds['sageGrammarPath']}
-    (conn, cursor) = db_connect(DATABASE['sage']['prod'])
-    return(cursor)
+    for dset in cursor:
+        DSDICT[dset['identifier']] = {'config': dset['sageConfigPath'],
+                                      'grammar': dset['sageGrammarPath']}
+    (CONN['sage'], CURSOR['sage']) = db_connect(DATABASE['sage']['prod'])
 
 
-def processImages(cursor):
-    mode = 'ALL' if ALL else 'OVERDUE'
+def process_images():
+    mode = 'ALL' if ARG.ALL else 'OVERDUE'
     stmt = SQL[mode]
-    if SLIDE:
-        addition = '%' + SLIDE + '%'
+    if ARG.SLIDE:
+        addition = '%' + ARG.SLIDE + '%'
         stmt = stmt.replace("sample')",
                             "sample') AND ips.value LIKE '" + addition + "'")
-    if DATASET:
-        addition = '%' + DATASET + '%'
+    if ARG.DATASET:
+        addition = '%' + ARG.DATASET + '%'
         stmt = stmt.replace("sample')",
                             "sample') AND ipd.value LIKE '" + addition + "'")
-    try:
-        logger.debug(stmt)
-        cursor.execute(stmt)
-    except MySQLdb.Error as e:
-        sql_error(e)
+    rows = list()
+    if ARG.IDS:
+        idfile = open(ARG.IDS, "r")
+        for line in idfile:
+            line = line.rstrip()
+            try:
+                CURSOR['sage'].execute(SQL['SINGLE'], (line, ))
+                row = CURSOR['sage'].fetchone()
+                rows.append(row)
+            except MySQLdb.Error as err:
+                sql_error(err)
+        idfile.close()
+    else:
+        try:
+            LOGGER.debug(stmt)
+            CURSOR['sage'].execute(stmt)
+            rows = CURSOR['sage'].fetchall()
+            print(rows)
+        except MySQLdb.Error as err:
+            sql_error(err)
 
     lsm = dict()
-    for (family, data_set, slide_code, name) in cursor:
+    for row in rows:
         config = ''
         grammar = ''
-        count['found'] += 1
-        if DEBUG:
-            print("%s\t%s\t%s\t%s" % (family, data_set, slide_code, name))
-        if family == 'rubin_chacrm':
+        COUNT['found'] += 1
+        LOGGER.info("%s\t%s\t%s\t%s", row['family'], row['data_set'],
+                    row['slide_code'], row['name'])
+        if row['family'] == 'rubin_chacrm':
             config = '/groups/scicompsoft/informatics/data/rubin_light_imagery-config.xml'
             grammar = '/misc/sc/pipeline/grammar/chacrm_sage.gra'
-        elif data_set in dsdict:
-            config = dsdict[data_set]['config']
-            grammar = dsdict[data_set]['grammar']
+        elif row['data_set'] in DSDICT:
+            config = DSDICT[row['data_set']]['config']
+            grammar = DSDICT[row['data_set']]['grammar']
             grammar = grammar.replace("/misc/local/pipeline", "/misc/sc/pipeline")
         else:
-            print('Could not determine configuration and grammar for data set %s' % (data_set))
-        lsm.setdefault(data_set, []).append(name)
-        if config and grammar and INDEX_ONLY:
-            indexImage(config, grammar, name, data_set)
+            LOGGER.error('Could not determine configuration and grammar '
+                         + 'for data set %s', row['data_set'])
+        lsm.setdefault(row['data_set'], []).append(row['name'])
+        if config and grammar and ARG.INDEX_ONLY:
+            index_image(config, grammar, row['name'], row['data_set'])
     operation = 'indexed'
-    if not INDEX_ONLY:
+    if not ARG.INDEX_ONLY:
         operation = 'indexed/discovered'
         for dataset, lsmlist in lsm.items():
-            logger.info("Running indexing/discovery on data set " + dataset + " with " + str(len(lsmlist)) + " LSM(s)")
+            LOGGER.info("Running indexing/discovery on data set " + dataset
+                        + " with " + str(len(lsmlist)) + " LSM(s)")
             if sys.version_info[0] == 2:
                 carr = xrange(0, len(lsmlist), 50)
             else:
@@ -147,99 +170,96 @@ def processImages(cursor):
             chunks = [lsmlist[i:i + 50] for i in carr]
             for sublist in chunks:
                 post = {"lsmNames": sublist}
-                logger.debug("  Posting " + str(len(sublist)) + " LSM(s)")
-                if TEST:
-                    count['success'] += len(sublist)
-                    indexed[dataset] = indexed.setdefault(dataset, 0) + len(sublist)
+                LOGGER.debug("  Posting " + str(len(sublist)) + " LSM(s)")
+                if ARG.TEST:
+                    COUNT['success'] += len(sublist)
+                    INDEXED[dataset] = INDEXED.setdefault(dataset, 0) + len(sublist)
                 else:
-                    response = call_responder('jacs', 'process/owner/system/dataSet/' + dataset + '/lsmPipelines', post)
-                    count['success'] += len(sublist)
-                    indexed[dataset] = indexed.setdefault(dataset, 0) + len(sublist)
-    print('Unindexed images: %d' % count['found'])
-    if count['skipped']:
-        print('Skipped images: %d' % count['skipped'])
-    print('Images successfully %s: %d' % (operation, count['success']))
-    if len(indexed):
-        for (ds) in sorted(indexed):
-            print('  %s: %d' % (ds, indexed[ds]))
-    if count['failure']:
-        print('Images failing indexing: %d' % count['failure'])
+                    call_responder('jacs', 'process/owner/system/dataSet/'
+                                   + dataset + '/lsmPipelines', post)
+                    COUNT['success'] += len(sublist)
+                    INDEXED[dataset] = INDEXED.setdefault(dataset, 0) + len(sublist)
+    print('Unindexed images: %d' % COUNT['found'])
+    if COUNT['skipped']:
+        print('Skipped images: %d' % COUNT['skipped'])
+    print('Images successfully %s: %d' % (operation, COUNT['success']))
+    if INDEXED:
+        for dset in sorted(INDEXED):
+            print('  %s: %d' % (dset, INDEXED[dset]))
+    if COUNT['failure']:
+        print('Images failing indexing: %d' % COUNT['failure'])
 
 
-def indexImage(config, grammar, name, data_set):
+def index_image(config, grammar, name, data_set):
     command = ['perl', '/groups/scicompsoft/informatics/bin/sage_loader.pl', '-config',
                config, '-grammar', grammar, '-item', name, '-lab',
                'flylight', '-verbose', '-description',
                'Image load from indexing_cleanup']
-    logger.info('Processing %s %s' % (data_set, name))
-    logger.debug('  ' + ' '.join(command))
+    LOGGER.info('Processing %s %s' % (data_set, name))
+    LOGGER.debug('  ' + ' '.join(command))
     try:
-        if TEST:
+        if ARG.TEST:
             tmp = 'OK'
-            logger.warning("Test mode: will not send transactions")
+            LOGGER.warning("Test mode: will not send transactions")
         else:
             tmp = subprocess.check_output(command, stderr=subprocess.STDOUT)
-        if (tmp.find('Cannot read file') != -1) or (tmp.find('Permission denied') != -1) or (tmp.find('Unable to uncompress the stack') != -1):
-            logger.error('  Could not read LSM file')
-            count['failure'] += 1
+        if (tmp.find('Cannot read file') != -1) or \
+           (tmp.find('Permission denied') != -1) or \
+           (tmp.find('Unable to uncompress the stack') != -1):
+            LOGGER.error('  Could not read LSM file')
+            COUNT['failure'] += 1
         elif tmp.find('Incomplete image processing') != -1:
-            logger.error('  Image processing failed')
-            count['failure'] += 1
+            LOGGER.error('  Image processing failed')
+            COUNT['failure'] += 1
         else:
-            count['success'] += 1
-            indexed[data_set] = indexed.setdefault(data_set, 0) + 1
+            COUNT['success'] += 1
+            INDEXED[data_set] = INDEXED.setdefault(data_set, 0) + 1
     except subprocess.CalledProcessError as err:
         print(err.output)
-        count['failure'] += 1
+        COUNT['failure'] += 1
     except Exception as err:
         print(err)
-        count['failure'] += 1
-    if DEBUG:
-        print('-' * 79)
+        COUNT['failure'] += 1
+
 
 # -----------------------------------------------------------------------------
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Find and index/discover newly tmogged imagery')
-    parser.add_argument('--data_set', dest='dataset', action='store',
+    PARSER = argparse.ArgumentParser(description='Find and index/discover newly tmogged imagery')
+    PARSER.add_argument('--ids', dest='IDS', action='store',
+                        help='File of image IDs (optional)')
+    PARSER.add_argument('--data_set', dest='DATASET', action='store',
                         help='Data set (optional)')
-    parser.add_argument('--slide_code', dest='slide', action='store',
+    PARSER.add_argument('--slide_code', dest='SLIDE', action='store',
                         help='Slide code (optional)')
-    parser.add_argument('--index', action='store_true', dest='index',
+    PARSER.add_argument('--index', action='store_true', dest='INDEX_ONLY',
                         default=False, help='Do not run discovery')
-    parser.add_argument('--verbose', action='store_true', dest='verbose',
+    PARSER.add_argument('--verbose', action='store_true', dest='VERBOSE',
                         default=False, help='Turn on verbose output')
-    parser.add_argument('--debug', action='store_true', dest='debug',
+    PARSER.add_argument('--debug', action='store_true', dest='DEBUG',
                         default=False, help='Turn on debug output')
-    parser.add_argument('--test', action='store_true', dest='test',
-                        default=False, help='Test mode - does not actually run the indexer or discovery service')
-    parser.add_argument('--all', action='store_true', dest='all',
-                        default=False, help='Selects all images, not just overdue ones')
-    args = parser.parse_args()
-    DATASET = args.dataset
-    SLIDE = args.slide
-    INDEX_ONLY = args.index
-    VERBOSE = args.verbose
-    DEBUG = args.debug
-    TEST = args.test
-    ALL = args.all
-    if DEBUG:
-        VERBOSE = True
-    logger = colorlog.getLogger()
-    if DEBUG:
-        logger.setLevel(colorlog.colorlog.logging.DEBUG)
-    elif VERBOSE:
-        logger.setLevel(colorlog.colorlog.logging.INFO)
+    PARSER.add_argument('--test', action='store_true', dest='TEST',
+                        default=False,
+                        help='Test mode - does not actually run the indexer or discovery service')
+    PARSER.add_argument('--all', action='store_true', dest='ALL',
+                        default=False,
+                        help='Selects all images, not just overdue ones')
+    ARG = PARSER.parse_args()
+    LOGGER = colorlog.getLogger()
+    if ARG.DEBUG:
+        LOGGER.setLevel(colorlog.colorlog.logging.DEBUG)
+    elif ARG.VERBOSE:
+        LOGGER.setLevel(colorlog.colorlog.logging.INFO)
     else:
-        logger.setLevel(colorlog.colorlog.logging.WARNING)
+        LOGGER.setLevel(colorlog.colorlog.logging.WARNING)
     HANDLER = colorlog.StreamHandler()
     HANDLER.setFormatter(colorlog.ColoredFormatter())
-    logger.addHandler(HANDLER)
+    LOGGER.addHandler(HANDLER)
 
     data = call_responder('config', 'config/db_config')
     DATABASE = data['config']
-    (cursor) = connect_databases()
+    connect_databases()
     data = call_responder('config', 'config/rest_services')
     CONFIG = data['config']
-    processImages(cursor)
+    process_images()
