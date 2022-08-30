0c772ecb7bc26bdc2e73359b68c0ce562750addb
robsv <svirskasr@janelia.hhmi.org>
2022-08-30 13:18:01 -0400

    Added prod manifold

--- bin/update_cross_info.sh
+++ bin/update_cross_info.sh
@@ -3,5 +3,5 @@
 slide_codes='processed_slide_codes.txt'
 for prop in cross_description effector_description lab_member lab_project
 do
-   python3 update_jacs_line.py --property $prop --manifold prod --file $slide_codes --write
+   python3 update_jacs_line.py --property $prop --manifold prod --file $slide_codes --manifold prod --write
 done
